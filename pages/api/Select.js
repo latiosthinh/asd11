d00bb6d6516ed9d480fdd831cb2f5f539169e80d
thomas.nguyen <latiosthinh@live.com>
2022-07-18 18:07:16 +0700

    adjust select padding

--- pages/api/Select.js
+++ pages/api/Select.js
@@ -2,7 +2,7 @@ function Select( {obj, cb} ) {
 	return (
 		<>
 			<label className="text-xl mr-5">{ obj.title }</label>
-			<select className="text-xl border-solid border-2 border-indigo-600 rounded-sm" onChange={ cb }>
+			<select className="px-3 py-1 text-xl border-solid border border-black rounded-sm" onChange={ cb }>
 				{ obj.data.map( d => <option key={ d } value={ d }>{ d }</option> ) }
 			</select>
 		</>
